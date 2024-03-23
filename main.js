a7f30a4dd3728b2e8eb0a38803f59fb21f9565f5
notdule <148584497+notdule@users.noreply.github.com>
2024-03-23 23:04:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -35,5 +35,11 @@ function addEntry(){
 function getCaloriesFromInputs(list) {
   let calories = 0;
   for (const item of list) {
+    const currVal = cleanInputString(item.value);
+    const invalidInputMatch = isInvalidInput(currVal);
 
+    if (invalidInputMatch) {
+
+    }
   }
+}
