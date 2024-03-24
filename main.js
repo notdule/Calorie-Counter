a8a5b59a62b3c6f22716a4287d93e3a95d4916b4
notdule <148584497+notdule@users.noreply.github.com>
2024-03-24 23:30:21 +0100

    Update invalid input function

--- main.js
+++ main.js
@@ -39,7 +39,13 @@ function getCaloriesFromInputs(list) {
     const invalidInputMatch = isInvalidInput(currVal);
 
     if (invalidInputMatch) {
-
+        alert(`Invalid Input: ${invalidInputMatch[0]}`);
+        isError = true;
+        return null;
     }
+    calories += Number(currVal);
   }
+  return calories;
 }
+
+addEntryButton.addEventListener("click", addEntry);
