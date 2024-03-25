4184300cf8644d0de21257943d5fcbeb367740fc
notdule <148584497+notdule@users.noreply.github.com>
2024-03-25 23:10:33 +0100

    Calculating Calories Function

--- main.js
+++ main.js
@@ -32,6 +32,19 @@ function addEntry(){
   targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
  } 
 
+function calculateCalories(e) {
+  e.preventDefault();
+  isError = false;
+
+  const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
+  const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
+  const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');
+  const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
+  const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
+
+
+}
+
 function getCaloriesFromInputs(list) {
   let calories = 0;
   for (const item of list) {
