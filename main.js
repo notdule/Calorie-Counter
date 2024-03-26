08688e4c7644955a60d6ba053509094a2df01d0f
notdule <148584497+notdule@users.noreply.github.com>
2024-03-26 23:20:31 +0100

    Update Calculate Calories Function

--- main.js
+++ main.js
@@ -42,6 +42,13 @@ function calculateCalories(e) {
   const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
   const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
 
+  const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+  const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+  const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+  const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+  const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+  const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+
 
 }
 
